b429d134feedabe8b83b569a7f1e6cadaa2101ff
Steve Phillips <elimisteve@gmail.com>
2012-02-04 22:43:33 -0800

    MUCH better. Very few problems left (just a couple paths)

--- builder.py
+++ builder.py
@@ -43,22 +43,24 @@ HOME_DIR = os.path.expandvars('$HOME').rstrip('/') + '/'
 
 # Trailing / may be included or excluded
 PROJECT_PATH = sys.argv[1].rstrip('/') + '/'
-BASE_PATH, PROJECT_NAME = [path[:-1], path[-1] for path in PROJECT_PATH.split('/')[:-1]]
-BASE_PATH = '/'.join(BASE_PATH)
+print sys.argv[1]
+print PROJECT_PATH
+PROJECT_NAME = PROJECT_PATH.split('/')[-2]
+BASE_PATH    = '/'.join(PROJECT_PATH.split('/')[:-2]) + '/'
 
 # Make virtualenv
 # FIXME Shouldn't assume the location of virtualenvwrapper.sh
-local('bash -c "source /usr/local/bin/virtualenvwrapper.sh && mkvirtualenv %s --no-site-packages"' %
-      (PROJECT_NAME))
+try:
+    local('bash -c "source /usr/local/bin/virtualenvwrapper.sh && mkvirtualenv %s --no-site-packages"' % (PROJECT_NAME))
+      #(pbs.which('virtualenvwrapper.sh'), ))
+except:
+    print "virtualenv probably created..."
+
       #(pbs.which('virtualenvwrapper.sh'), ))
 ##VIRTUALENV_PATH = HOME_DIR + '.virtualenvs/' + PROJECT_NAME
 
-# Make directories
-# FIXME Add more dirs to this list
-for dir_name in ['', 'media', 'static', 'templates', 'apache', 'cms_settings']:
-    os.mkdir(PROJECT_PATH + dir_name)
-
 SECRET_KEY = ''.join([ random.choice(string.printable[:94].replace("'", "")) for _ in range(50) ])
+PROJECT_PASSWORD = ''.join([ random.choice(string.printable[:67].replace("'", "")) for _ in range(30) ])
 
 replacement_values = {
     'PROJECT_NAME':     PROJECT_NAME,
@@ -67,20 +69,27 @@ replacement_values = {
     'SECRET_KEY':       SECRET_KEY,
 }
 
+# Make directories
+# FIXME Add more dirs to this list
+for dir_name in ['', 'media', 'static', 'templates', 'apache', 'cms_settings',
+                 '%(PROJECT_NAME)s']:
+    os.mkdir(PROJECT_PATH + dir_name % replacement_values)
+
+
 generic_files = [x for x in os.listdir(GENERIC_SCRIPTS_PATH)
                  if x.endswith('-generic')]
 
 for filename in generic_files:
     # Grab *-generic filenames
     f_read = open(GENERIC_SCRIPTS_PATH + filename, 'r')
-    contents = f.read()
-    f.close()
+    contents = f_read.read()
+    f_read.close()
 
     # Replace %(SECRET_KEY)s, etc with new value for new project
     new_filename = filename.replace('-generic', '')
     # Path names include '%(PROJECT_NAME)s', etc
     file_path = pathify[new_filename] % replacement_values
-    f_write = open(PROJECT_PATH + , 'w')
+    f_write = open(PROJECT_PATH + file_path + new_filename, 'a')
     new_contents = contents % replacement_values
     f_write.write(new_contents)
     f_write.close()
