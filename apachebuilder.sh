868f80948d5e24e1ef930a01a5fe35708cff91c6
Steve Phillips <elimisteve@gmail.com>
2012-02-11 00:45:37 -0800

    apachebuilder.sh: improved instructions, silenced misleading Apache message

--- apachebuilder.sh
+++ apachebuilder.sh
@@ -17,5 +17,7 @@ fi
 # Generate new Apache config
 python proto-new-virtualhost-subdomain.py $PROJECT_NAME > /etc/apache2/sites-available/$PROJECT_NAME
 # Create symlink from sites-available to sites-enabled
-a2ensite $PROJECT_NAME
+a2ensite $PROJECT_NAME 2>&1 | grep -v .
 #cd /etc/apache2/sites-available/ && ln -s $PROJECT_NAME ../sites-enabled/$PROJECT_NAME
+echo
+echo "Push to $PROJECT_NAME/bare/, then restart apache"
