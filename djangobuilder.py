8f79bf409ab868b5e9fef4117bd5038ef0e774ab
AJ Bahnken <avb.wkyhu@gmail.com>
2012-02-18 15:24:40 -0800

    Argparse integration is fully functional

--- djangobuilder.py
+++ djangobuilder.py
@@ -44,7 +44,7 @@ cms_options.add_argument('--zinnia', action='store_true', default=False,
 arguments = parser.parse_args()
 
 
-# FIXME Every file in generic_scripts and *-generic should be listed
+# FIXME Every file in generic_scripts and *-needed should be listed
 # here... or we can copy entire directories
 pathify = {
     '.gitignore':        ['%(PROJECT_NAME)s/'],
@@ -110,28 +110,29 @@ for dir_name in ['', 'static', 'apache', 'extra_settings',
 
 
 generic_files = [x for x in os.listdir(GENERIC_SCRIPTS_PATH)
-                 if x.endswith('-generic')]
+                 if x.endswith('-needed')]
 
 #This adds cms_settings/zinnia_settings
 #to generic_files.
 ##FIXME shouldn't be hard coded
-#if arguments.cms == True or arguments.zinnia == True:
-#    extra_files = [x for x in os.listdir('extra_settings/')
-#                      if x.startswith('cms')]
-#if arguments.zinnia == True:
-#    extra_files += [x for x in os.listdir('extra_settings/')
-#                      if x.startswith('zinnia')]
+if arguments.cms == True or arguments.zinnia == True:
+    generic_files = [x for x in os.listdir('extra_settings/')
+                      if x.startswith('cms')]
+if arguments.zinnia == True:
+    generic_files += [x for x in os.listdir('extra_settings/')
+                      if x.startswith('zinnia')]
 
 
 print "Creating files..."
 for filename in generic_files:
-    # Grab *-generic filenames
+    # Grab *-needed filenames
     f_read = open(GENERIC_SCRIPTS_PATH + filename, 'r')
     contents = f_read.read()
     f_read.close()
 
     # Replace %(SECRET_KEY)s, etc with new value for new project
-    new_filename = filename.replace('-generic', '')
+    if filename.endswith('-needed'):
+        new_filename = filename.replace('-needed', '')
     # Loop through list of locations new_filename should be placed
     for dir in pathify[new_filename]:
         # Path names include '%(PROJECT_NAME)s', etc
