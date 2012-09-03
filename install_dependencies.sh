6f7c425c1b36c61bad20694cd4bc06121cd1a4b0
Kevin Xu <kevin@imkevinxu.com>
2012-09-03 04:48:22 -0700

    updated install_dependencies with better checks

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -6,21 +6,38 @@
 # Tested on a Mac OSX 10.7 Lion
 
 # Installing pip, django, virtualenv, virtualenvwrapper
-echo "Installing pip..."
-sudo easy_install pip
-echo "Installing django..."
-sudo pip install django
-echo "Installing virtualenv..."
-sudo pip install virtualenv
-echo "Installing virtualenvwrapper..."
-sudo pip install virtualenvwrapper
+if [ -z `which pip` ]
+    then
+        echo "Installing pip..."
+        sudo easy_install pip
+fi
+
+if [ -z `which django-admin.py` ]
+    then
+        echo "Installing django..."
+        sudo pip install django
+fi
+
+if [ -z `which virtualenv` ]
+    then
+        echo "Installing virtualenv..."
+        sudo pip install virtualenv
+fi
+
+if [ -z `which virtualenvwrapper.sh` ]
+    then
+        echo "Installing virtualenvwrapper..."
+        sudo pip install virtualenvwrapper
+fi
 
 # Adds lines to the shell startup file so that virtualenvwrapper can work
 # http://virtualenvwrapper.readthedocs.org/en/latest/install.html#shell-startup-file
-echo "Adding virtualenvwrapper variables to ~/.bash_profile"
-echo source `which virtualenvwrapper.sh` >> ~/.bash_profile
-echo "export WORKON_HOME=$HOME/.virtualenvs" >> ~/.bash_profile
-source ~/.bash_profile
+if [ ! -d "$WORKON_HOME" ]
+    then
+        echo "Adding virtualenvwrapper variables to ~/.bash_profile"
+        echo source `which virtualenvwrapper.sh` >> ~/.bash_profile
+        source ~/.bash_profile
+fi
 
 # Fixes problem that occurs on OSX 10.7 Lion when making a new virtualenv,
 # it goes looking for disutils but crashes if it can't find one
@@ -33,3 +50,5 @@ if [ -z `which git` ]
         echo "Detected git has not been installed"
         echo "INSTALL git here http://git-scm.com/downloads"
 fi
+
+echo "All dependencies installed!"
